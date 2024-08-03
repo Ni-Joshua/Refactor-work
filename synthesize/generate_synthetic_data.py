922f66333ee3524fdeee8341473c238fb9d97ab0
Enderwaraxe <icewolf0221@gmail.com>
2024-08-02 22:21:26 -0700

    First draft of complete generate_defective()

--- synthesize/generate_synthetic_data.py
+++ synthesize/generate_synthetic_data.py
@@ -37,17 +37,27 @@ class Synthesizer:
                  neighborcount=3, rate = 0.3):
         np.random.seed(1234)
         self.synthetic_data_info = synthetic_data_info
-        self.clean_means_variances = clean_means_variances
+        self.variances = clean_means_variances['covariance']
+        self.means = clean_means_variances['means']
+        self.attributes = []
+        for i in range(0, len(self.means[0])):
+            attributename = "attr"+str(i+1)
+            self.attributes.append(attributename)
         self.neighborcount = neighborcount
         self.rate = rate
-        self.clean_data = None
+        self.completeClean_data = []
+
+        self.clean_data = []
+        self.missing_data = []
+        self.extreme_data = []
+        self.ood_data = []
         self.synthetic_data = self.synthetic_data_info.drop(["CID", "geometry"], axis=1)
         self.synthetic_data = self.synthetic_data.dropna()
         self.synthetic_data["x"] = self.synthetic_data["x"] - np.mean(self.synthetic_data['x'])
         self.synthetic_data["y"] = self.synthetic_data["y"] - np.mean(self.synthetic_data['y'])
         self.synthetic_data["geometry"] = gpd.points_from_xy(x=self.synthetic_data.x, y=self.synthetic_data.y)
 
-    def get_attr(self, groups, means,variances):
+    def generate_attr(self, groups, means,variances):
         attrvalues = []
         for index in range(0, len(groups)):
             attrvalues.append(np.random.multivariate_normal(means[groups[index]-1], variances[groups[index]-1]))
@@ -70,39 +80,49 @@ class Synthesizer:
     
     def generate_clean(self):
         synthetic_data = self.synthetic_data
-        attributes = []
-        variances = self.clean_means_variances['covariance']
-        means = self.clean_means_variances['means']
-        for i in range(0, len(means[0])):
-            attributename = "attr"+str(i+1)
-            attributes.append(attributename)
-        synthetic_data[attributes] = self.get_attr(synthetic_data['clus_group'], means, variances)
+
+        synthetic_data[self.attributes] = self.generate_attr(synthetic_data['clus_group'], self.means, self.variances)
         
         nearest_pt, n_pt_cols = self.get_neighbors(synthetic_data)
         synthetic_data = synthetic_data.join(nearest_pt)
-        complete_data = synthetic_data[attributes+n_pt_cols]
-        self.clean_data = synthetic_data
-        complete_data.to_csv(r'synthetic_data_clean.txt', header=None, index=True, sep=',')
+        self.clean_data = np.array(synthetic_data[self.attributes])
+        self.completeClean_data = synthetic_data
 
-    def generate_defective(self, X, mu, Sigma, rate=0.1):
-        N, d = X.shape
-        mask = np.random.choice([True, False], (N, d), p=[rate, 1-rate])
+    def generate_defective(self):
+        clusters = list(set(self.completeClean_data['clus_group']))
+        for cluster in clusters:
+            mu = self.means[cluster-1]
+            Sigma = self.variances[cluster-1]
+            clusterattrvals = self.completeClean_data.iloc[self.completeClean_data.groupby('clus_group').indices.get(cluster)][self.attributes]
+            clusterattrvals = np.array(clusterattrvals)
+            Npoints, attrcount = clusterattrvals.shape
 
-        if mode == "missing":
-            X_missing = copy.deepcopy(X)
+            mask = np.random.choice([True, False], (Npoints, attrcount), p=[self.rate, 1-self.rate])
+
+            X_missing = copy.deepcopy(clusterattrvals)
             X_missing[mask] = np.nan
-        elif mode == "extreme":
-            X_extreme = copy.deepcopy(X)
-            defects = 3 * np.diagonal(Sigma).reshape((1,-1)) * np.random.choice([1,-1], (N,d)) + multivariate_normal(mean=mu, cov=Sigma)
-            X_extreme[mask] = defects[mask]
-        elif mode == "ood":
-            X_ood = copy.deepcopy(X)
-            defects = mu.reshape((1,-1)) * poisson.rvs(1, size=(N, d))
-            X_ood[mask] = defects[mask]
-        else:
-            assert False, "Unknown defect mode!"
 
-        return X_missing, X_extreme, X_ood
+            X_extreme = copy.deepcopy(clusterattrvals)
+            defects = 3 * np.diagonal(Sigma).reshape((1,-1)) * np.random.choice([1,-1], (Npoints,attrcount)) + np.random.multivariate_normal(mean=mu, cov=Sigma)
+            X_extreme[mask] = defects[mask]
+
+            X_ood = copy.deepcopy(clusterattrvals)
+            defects = mu.reshape((1,-1)) * poisson.rvs(1, size=(Npoints, attrcount))
+            X_ood[mask] = defects[mask]
+
+
+            self.missing_data.extend(X_missing)
+            self.extreme_data.extend(X_extreme)
+            self.ood_data.extend(X_ood)
+        self.missing_data = np.array(self.missing_data)
+        self.extreme_data = np.array(self.extreme_data)
+        self.ood_data = np.array(self.ood_data)
+    
+    def generate_all(self):
+        self.generate_clean()
+        self.generate_defective()
+        np.savez("All-synthetic-data.npz", gt= self.synthetic_data['clus_group'], clean_data = self.clean_data, missing_data = self.missing_data, extreme_data=self.extreme_data, ood_data = self.ood_data, position = np.array([self.synthetic_data['x'], self.synthetic_data['y']]))
+        # self.completeClean_data.to_csv(r'synthetic_data_clean.txt', header=None, index=True, sep=',')
 
     def display_gt(self):
         fig, ax = plt.subplots(figsize=(10, 5))
@@ -110,3 +130,47 @@ class Synthesizer:
         ax.set_axis_off()
         ax.title.set_text('Ground Truth')
         plt.show()
+
+    def display_clean(self):
+        for i in range(0, len(self.attributes)):
+            fig, ax = plt.subplots(figsize=(10, 5))
+            plt.scatter(self.synthetic_data['x'], self.synthetic_data['y'], c=self.clean_data[:, i], cmap="Set1")
+            ax.set_axis_off()
+            ax.title.set_text('Clean Data, attr ' + str(i+1))
+        plt.show()
+
+    def display_missing(self):
+        for i in range(0, len(self.attributes)):
+            fig, ax = plt.subplots(figsize=(10, 5))
+            plt.scatter(self.synthetic_data['x'], self.synthetic_data['y'], c=self.missing_data[:, i], cmap="Set1")
+            ax.set_axis_off()
+            ax.title.set_text('Missing Defective Data, attr ' + str(i+1))
+        plt.show()
+
+    def display_extreme(self):
+        for i in range(0, len(self.attributes)):
+            fig, ax = plt.subplots(figsize=(10, 5))
+            plt.scatter(self.synthetic_data['x'], self.synthetic_data['y'], c=self.extreme_data[:, i], cmap="Set1")
+            ax.set_axis_off()
+            ax.title.set_text('Extreme Defective Data, attr ' + str(i+1))
+        plt.show()
+    
+    def display_ood(self):
+        for i in range(0, len(self.attributes)):
+            fig, ax = plt.subplots(figsize=(10, 5))
+            plt.scatter(self.synthetic_data['x'], self.synthetic_data['y'], c=self.ood_data[:, i], cmap="Set1")
+            ax.set_axis_off()
+            ax.title.set_text('OOD Defective Data, attr ' + str(i+1))
+        plt.show()
+    
+    def display_all(self):
+        self.display_clean()
+        self.display_missing()
+        self.display_extreme()
+        self.display_ood()
+
+
+
+# temp = Synthesizer()
+# temp.generate_all()
+# temp.display_all()
